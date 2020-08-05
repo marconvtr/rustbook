95c4ec0e80e5a3093ea40b9fb574f0ba87418955
Victor Marcon <marconvtr@gmail.com>
2020-08-04 23:14:04 -0300

    guessing game complete wiwith extras

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
     loop{
         println!("Please input your guess.");
 
@@ -16,7 +15,11 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
+        if guess.trim() == "quit"
+        {
+            println!("Good bye!");
+            break;
+        }
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
