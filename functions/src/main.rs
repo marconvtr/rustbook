31e8a02cc84dcb9034510f63bab83ffc3e845754
Victor Marcon <marconvtr@gmail.com>
2020-08-08 16:21:54 -0300

    ownership

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
 
-    let a = [10,20,30,40,50];
-    let mut index = 0;
-
-    while index < 5 {
-        println!("O valor é: {}", a[index]);
-
-        index += 1;
+    for number in (1..4).rev() {
+        println!("{}!",number);
     }
+
+    println!("GO!");
 }
