1bf4dda29f130bc6b4fea6dcaae1b70f48f02b34
Victor Marcon <marconvtr@gmail.com>
2020-08-14 20:23:57 -0300

    structs finished

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -13,6 +13,12 @@ impl Rectangle{
     fn can_hold(&self,other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+    fn square(size: u32) -> Rectangle{
+        Rectangle{
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -28,9 +34,10 @@ fn main() {
         width: 60,
         height: 45,
     };
-
+    let rect4 = Rectangle::square(30);
     println!("Can rect1 hold rect2? {}",rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}",rect1.can_hold(&rect3));
+    println!("Area of square is {}",rect4.area())
 
 
 
